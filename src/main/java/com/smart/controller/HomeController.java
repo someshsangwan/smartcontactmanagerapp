b3a62a89ecfec685b1e0dd16410d83aa9cbda17f
somesh.sangwan <somesh.sangwan@rakuten.com>
2024-06-09 10:29:56 +0900

    home page creation

--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.smart.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -8,8 +9,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+    	model.addAttribute("title","Home-Smart Contact Manager");
         return "home";
+    }
+    @GetMapping("/about")
+    public String about(Model model) {
+    	model.addAttribute("title","About-Smart Contact Manager");
+        return "about";
     }
 }
 
